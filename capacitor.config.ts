38ead0f641de6956410b72920a44a38d42946b0d
Jerome <jeromeogsonar@gmail.com>
2025-11-30 15:14:01 +0800

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'dec-2025-ioni',
+  webDir: 'www'
+};
+
+export default config;
