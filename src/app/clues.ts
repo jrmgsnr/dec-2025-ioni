c5ba437be8b3c050bb0b766b97dfc1b5a838483d
Jerome <jeromeogsonar@gmail.com>
2025-11-30 21:33:14 +0800

    create clue decoder

---
+++ src/app/clues.ts
@@ -0,0 +1,47 @@
+export const CLUES = [
+  {
+    code: "017594",
+    clue: "Neither Greg nor the competitor who finished in 23 minutes was the competitor in the gray shirt.",
+    isSpecial: false
+  },
+  {
+    code: "482930",
+    clue: "Herman wasn't from Pierson.",
+    isSpecial: false
+  },
+  {
+    code: "982034",
+    clue: "Of the competitor who finished in 24 minutes and the competitor in the lime shirt, one was from Pierson and the other was Ralph.",
+    isSpecial: false
+  },
+  {
+    code: "284051",
+    clue: "The runner from Templeton finished in 21 minutes.",
+    isSpecial: true
+  },
+  {
+    code: "639285",
+    clue: "Neither the contestant in the gray shirt nor the runner from Alpine was Ralph.",
+    isSpecial: false
+  },
+  {
+    code: "502118",
+    clue: "Of Ralph and the competitor who finished in 22 minutes, one wore the red shirt and the other was from Rosie.",
+    isSpecial: false
+  },
+  {
+    code: "930476",
+    clue: "The runner from Rosie wore the aquamarine shirt.",
+    isSpecial: false
+  },
+  {
+    code: "715903",
+    clue: "Matthew is from Pierson",
+    isSpecial: true
+  },
+  {
+    code: "067842",
+    clue: "The runner wearing lime shirt finished in 23 minutes",
+    isSpecial: true
+  },
+]
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,75 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { IonicModule, ToastController } from '@ionic/angular';
+import { FormControl, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import * as _ from 'lodash';
+
+import { CLUES } from '../clues';
+
+interface Clue {
+  code: string,
+  clue: string,
+  isSpecial: boolean
+}
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [
+    CommonModule,
+    IonicModule,
+    FormsModule,
+    ReactiveFormsModule
+  ]
 })
 export class HomePage {
-  constructor() {}
+  code = new FormControl('', [
+    Validators.required,
+    Validators.minLength(6),
+    Validators.maxLength(6)
+  ]);
+
+  unlockedClues: Clue[] = [];
+
+  constructor(
+    private toastCtrl: ToastController
+  ) {
+    let cachedData = localStorage.getItem('unlockedClues'); 
+
+    if (cachedData) {
+      this.unlockedClues = JSON.parse(cachedData);
+    }
+  }
+
+  get isFormValid() {
+    return this.code.valid;
+  }
+
+  inputChecker(e: any) {
+    e.target.value = e.target.value.replace(/\D/g, '');
+  }
+
+  async showCode() {
+    const inputCode = this.code.value ?? '';
+    const clue = _.find(CLUES, { code: inputCode });
+    const exists = _.some(this.unlockedClues, clue);
+
+    console.log('DEBUG', this.unlockedClues, clue)
+
+    if (clue && !exists) {
+      this.unlockedClues.unshift(clue);
+      localStorage.setItem('unlockedClues', JSON.stringify(this.unlockedClues));
+    } else {
+      const toast = this.toastCtrl.create({
+        message: clue && exists ? 'Clue already unlocked' : 'Invalid code',
+        duration: 1000,
+        position: 'middle'
+      });
+
+      (await toast).present();
+    }
+
+    this.code.reset();
+  }
 }
